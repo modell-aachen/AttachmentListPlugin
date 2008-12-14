0696ed3dada1b618e0e17858b871455f63207e06
OlivierRaginel <OlivierRaginel@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2008-12-14 17:49:56 +0000

    Item27: Fix svn properties on all .pm, .tmpl and .txt files
    git-svn-id: http://svn.foswiki.org/trunk/AttachmentListPlugin@1340 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- lib/TWiki/Plugins/AttachmentListPlugin.pm
+++ lib/TWiki/Plugins/AttachmentListPlugin.pm
@@ -30,10 +30,10 @@ my %sortInputTable = (
       $TWiki::Plugins::TopicDataHelperPlugin::sortDirections{'DESCENDING'},
 );
 
-# This should always be $Rev: 14207 $ so that TWiki can determine the checked-in
+# This should always be $Rev$ so that TWiki can determine the checked-in
 # status of the plugin. It is used by the build automation tools, so
 # you should leave it alone.
-$VERSION = '$Rev: 14207 $';
+$VERSION = '$Rev$';
 
 # This is a free-form string you can use to "name" your own plugin version.
 # It is *not* used by the build automation tools, but is reported as part
